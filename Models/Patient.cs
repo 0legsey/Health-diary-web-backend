beab6af58d253bf2979d44190096b2b554d74ec4
Nikita <nik_naydenko@mail.ru>
2025-10-14 14:29:01 +0700

    Added Patient model and set up one to many doctor-patient relationship

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -3,11 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Models
 {
-    public class Doctor
+    public class Patient
     {
         public int Id { get; set; }
-        [MaxLength(100)]
-        public string LicenceNumber { get; set; } = string.Empty;
         [MaxLength(100)]
         public string Email { get; set; } = string.Empty;
         [MaxLength(50)]
@@ -22,10 +20,11 @@ namespace Backend.Models
         public DateTime BirthDate { get; set; }
         [Column(TypeName = "timestamp without time zone")]
         public DateTime CreatedAt { get; set; }
-        [MaxLength(100)]
-        public string Specialization { get; set; } = string.Empty;
-        [MaxLength(50)]
-        public string Experience { get; set; } = string.Empty;
-        public List<Patient> Patients { get; set; } = new List<Patient>();
+        [MaxLength(255)]
+        public string ChronicDeseases { get; set; } = string.Empty;
+        [MaxLength(255)]
+        public string Allergies { get; set; } = string.Empty;
+        public int? DoctorId { get; set; }
+        public Doctor? Doctor { get; set; } //Navigation property
     }
 }
