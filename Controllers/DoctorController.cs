1d6091c8338e0065aad3bb5ce4476c2dd90f70fe
Nikita <nik_naydenko@mail.ru>
2025-10-14 15:31:29 +0700

    Added Doctor controller

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,35 @@
+﻿using Backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DoctorController : ControllerBase
+    {
+        private readonly ApplicationDBContext _applicationDBContext;
+        public DoctorController(ApplicationDBContext applicationDBContext)
+        {
+            _applicationDBContext = applicationDBContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var doctors = _applicationDBContext.Doctors.ToList();
+
+            return Ok(doctors);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) {
+            var stock = _applicationDBContext.Doctors.Find(id);
+
+            if (stock == null) {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}
