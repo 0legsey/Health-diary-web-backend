c692bbf92dfa9dcaa70dc8394aa410c2a1078b4d
Nikita <nik_naydenko@mail.ru>
2025-10-14 14:12:47 +0700

    Added Doctor model

---
+++ Models/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string LicenceNumber { get; set; } = string.Empty;
+        [MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+        [MaxLength(100)]
+        public string PasswordHash { get; set; } = string.Empty;
+        [MaxLength(30)]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Column(TypeName = "timestamp without time zone")]
+        public DateTime BirthDate { get; set; }
+        [Column(TypeName = "timestamp without time zone")]
+        public DateTime CreatedAt { get; set; }
+        [MaxLength(100)]
+        public string Specialization { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string Experience { get; set; } = string.Empty;
+    }
+}
